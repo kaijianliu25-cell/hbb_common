848adda050e7a2b71dc4b2be1952910d0ac56f4c
kaijianliu25-cell <kaijianliu25@gmail.com>
2026-04-24 12:32:59 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["159.75.205.125"];
+pub const RS_PUB_KEY: &str = "	px3kPEhY4sbzRNOKnhLKKzJtGNdZ1D0ermuZ+X9zJsM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
